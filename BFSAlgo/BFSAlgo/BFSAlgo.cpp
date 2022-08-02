c68e6a002d6845d4338ff9735646f01f54e93007
tamellLUU <trevor.amell@activision.com>
2022-08-02 17:28:52 -0400

    Modified BFS Challenge
    - Function now outputs the path to get from a starting node to the goal node. This is done by doing BFS traversal, but keep a dictionary of visited nodes and their previous node. This map can be used to walk backwards through to get the path from finish to start.
    
    - Can be cleaned up a bit, but moreso just happy to have a finished and working product for it.

--- BFSAlgo/BFSAlgo/BFSAlgo.cpp
+++ BFSAlgo/BFSAlgo/BFSAlgo.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <map>
 
 using namespace std;
 
-void BFS(int startingNode, vector<vector<int>>& adjMatrix);
+void BFS(int startingNode,int goalNode, vector<vector<int>>& adjMatrix);
+char letterEquiv(int val);
 
 int main()
 {
-    // BFS doesn't deal with weights, default the connects to 1
+    // BFS doesn't deal with weights, default the example connections to 1
     vector<vector<int>> adjMatrix
     {
         {0,1,0,1},
@@ -17,39 +19,76 @@ int main()
         {1,0,1,0}
     };
 
-    cout << "BFS Order: \n";
-    BFS(0, adjMatrix);
+    // From A, Find C
+    BFS(0, adjMatrix.size()-2, adjMatrix);
 }
 
-void BFS(int startingNode, vector<vector<int>>& adjMatrix)
+void BFS(int startingNode, int goalNode, vector<vector<int>>& adjMatrix)
 {
+    cout << "BFS From Node " << letterEquiv(startingNode) << " to Node " << letterEquiv(goalNode) << endl;
+
     // Create Visited Vector to hold if that 'node' has been visited
     vector<bool> visited;
     visited.resize(adjMatrix.size(), false);
 
+    // Create a map to pair node with it's previous node
+    map<int, int> cameFrom;
+    cameFrom[startingNode] = NULL;
+
     // Create a queue to enqueue adjacent nodes
     queue<int> queue;
 
+    int currentNode = startingNode;
+
     // Starting Node set to true and push to queue
-    visited[startingNode] = true;
-    queue.push(startingNode);
+    visited[currentNode] = true;
+    queue.push(currentNode);
 
     while (!queue.empty())
     {
-        startingNode = queue.front();
-
-        // Show Letter equivalent to vector position (0 = A, 1 = B...)
-        cout << (char)(startingNode + 65) << " ";
+        currentNode = queue.front();
         queue.pop();
 
+        // Check all adjacent Nodes
         for (int i = 0; i < adjMatrix.size(); i++)
         {
-            if (adjMatrix[startingNode][i] != 0 && !visited[i])
+            // If there's a connection and it's not visited
+            if (adjMatrix[currentNode][i] != 0 && !visited[i])
             {
+                // Visit, Put on Queue, and Map to Previous Node
                 visited[i] = true;
                 queue.push(i);
+                cameFrom[i] = currentNode;
             }
         }
     }
+
+    // Find Goal Node
+    currentNode = goalNode;
+    vector<int> path;
+
+    // Work backwords from goalNode and look at previous Node mappings to startingNode
+    while (currentNode != startingNode)
+    {
+        path.push_back(currentNode);
+        currentNode = cameFrom[currentNode];
+    }
+
+    // Push final Node and Reverse vector
+    path.push_back(currentNode);
+    reverse(path.begin(), path.end());
+
+    // Print
+    for (auto i : path)
+    {
+        cout << letterEquiv(i);
+        if(i != path[path.size()-1]) cout << " -> ";
+    }
     
+}
+
+// Misc Function to showcase letter equivalents for array positions
+char letterEquiv(int val)
+{
+    return (char)(val + 65);
 }
