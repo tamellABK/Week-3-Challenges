5e208a9e23d032fa824ac2b6afb9b4dc4bdb72ea
tamellLUU <trevor.amell@activision.com>
2022-08-03 17:10:11 -0400

    Modified SortingAlgorithms
    - User Input Validation
    - Structure Cleanup

--- SortingAlgorithms/SortingAlgorithms/SortingAlgorithms.cpp
+++ SortingAlgorithms/SortingAlgorithms/SortingAlgorithms.cpp
@@ -3,21 +3,25 @@
 
 using namespace std;
 
+int const RAND_UPPER_BOUND = 10000;
+
+int UserInput();
+void InvalidInputEvent();
+
+void InsertionSort(int arrayToSort[], int sizeOfArray);
 void PopulateArray(int generatedArray[], int sizeOfArray);
 void OuputArray(int generatedArray[], int sizeOfArray);
-void InsertionSort(int arrayToSort[], int sizeOfArray);
+
 
 int main()
 {
-    int arraySize;
     chrono::steady_clock::time_point timerStart, timerEnd;
     chrono::duration<double> elapsedTime;
 
+    int arraySize = UserInput();
+
     // Random seed based on time since epoch
     srand(chrono::steady_clock::now().time_since_epoch().count());
-
-    cout << "Choose size of the array to sort: ";
-    cin >> arraySize;
 
     int* generatedArray = new int[arraySize];
     PopulateArray(generatedArray, arraySize);
@@ -25,6 +29,7 @@ int main()
     cout << "\nOriginal Array Layout\n";
     OuputArray(generatedArray, arraySize);
 
+    // Time function in seconds
     timerStart = chrono::steady_clock::now();
     InsertionSort(generatedArray, arraySize);
     timerEnd = chrono::steady_clock::now();
@@ -35,8 +40,31 @@ int main()
     elapsedTime = timerEnd - timerStart;
     cout << "\nSorting Elapsed Time: " << elapsedTime.count() << " Seconds!\n";
 
+    // Array Cleanup
     delete[] generatedArray;
     generatedArray = nullptr;
+}
+
+int UserInput()
+{
+    int userGivenSize;
+    while (true)
+    {
+        cout << "Choose size of the array to sort: ";
+        cin >> userGivenSize;
+
+        if (!cin.fail()) break;
+        InvalidInputEvent();
+    }
+    return userGivenSize;
+}
+
+void InvalidInputEvent()
+{
+    cin.clear();
+    cin.ignore(99999, '\n');
+    system("cls");
+    cout << "Invalid Input!" << endl;
 }
 
 void InsertionSort(int arrayToSort[], int sizeOfArray)
@@ -64,7 +92,7 @@ void PopulateArray(int generatedArray[], int sizeOfArray)
     for (int i = 0; i < sizeOfArray; i++)
     {
         // Fill array with random numbers between 1 and 1000
-        generatedArray[i] = rand() % 1000 + 1;
+        generatedArray[i] = rand() % RAND_UPPER_BOUND + 1;
     }
 }
 
